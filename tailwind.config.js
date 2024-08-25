9b03b828dbb45b0ae3c7ce2db1cfd6f75f4865dd
tom <tomchenforwork@gmail.com>
2024-08-26 02:36:39 +0800

    feat: index+optical

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,13 +31,30 @@ module.exports = {
       "2xl": "1400px",
     },
     extend: {
+      backgroundImage: {
+        "sm-banner":
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-header-sm.png?raw=true')",
+        banner:
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-header.png?raw=true')",
+        "sm-glasses":
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/index-bg-sm.png?raw=true')",
+        glasses:
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/index-bg.png?raw=true')",
+        "sm-product-left":
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/product-header-sm-1.png?raw=true')",
+        "product-left":
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/product-header-1.png?raw=true')",
+        "sm-product-right":
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/product-header-sm-2.png?raw=true')",
+        "product-right":
+          "url('https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/product-header-2.png?raw=true')",
+      },
       colors: {
-        "light-bg": "#F8F9FA",
-        primary: "#198754",
-        secondary: "#6C757D",
-        text: "#212529",
-        danger: "#DC3545",
-        "border-line": "#CED4DA",
+        primary: "#AA0601",
+        secondary: "#FBF2F2",
+        "primary-dark": "#650300",
+        "glasses-dark-brown": "#5F3E2D",
+        "glasses-brown": "#B75929",
       },
     },
   },
